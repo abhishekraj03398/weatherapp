8e9a0020320e4519f650a6f2be6c2b31931c672e
abhishekraj03398 <abhishekraj03398@gmail.com>
2023-04-15 19:25:11 +0530

    push all weather files

---
+++ src/components/Weather.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import './WeatherStyle.css';
+import axios from 'axios';
+
+
+const Weather = () => {
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState('')
+  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=16fc15e25420fcd6395dc8ce9adc9b01`
+  
+   
+      const searchLocation = (event) => {
+        if (event.key === 'Enter') {
+          axios.get(url).then((response) => {
+            setData(response.data)
+          })
+          setLocation('')
+        } 
+      };
+
+   
+    
+  
+    return (
+      <div className="app">
+        <div className="search">
+          <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="search" />
+        </div>
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+            </div>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
+          </div>
+  
+             <div className="bottom">
+              <div className="feels">
+                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+  
+
+
+export default Weather;
